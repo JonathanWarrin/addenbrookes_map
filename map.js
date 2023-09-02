6dfd80e13f6ff4213409a2cb8831511091ae0a8b
Jonathan Warrin <jonathanwarrin@Jonathans-MacBook-Air-5.local>
2023-09-02 20:26:35 +0100

    changed marker

--- map.js
+++ map.js
@@ -2,12 +2,33 @@ let map, markers = [];
 
 const aspectRatio = 1; // This sets the aspect ratio to 1
 
+const myIcon = L.icon({
+    iconUrl: 'circle_blue_black.png',
+    iconSize: [60, 60] // You can set the size as per your need
+});
+
 function initMap() {
     map = L.map('map');
     const bounds = calculateBounds();
     const imageLayer = L.imageOverlay('level_2.png', bounds).addTo(map);
     map.fitBounds(imageLayer.getBounds()); // Ensure map fits to the bounds of the image
+
+    // Add Leaflet-specific event listeners
     addEventListeners();
+
+    // Add click events to the location list items (assuming they exist in your HTML)
+    const locationItems = document.querySelectorAll('#location-list li');
+    locationItems.forEach(item => {
+        item.addEventListener('click', function () {
+            const coords = JSON.parse(this.getAttribute('data-coordinates'));
+            addMarkerToMap(coords);
+        });
+    });
+}
+
+// Function to add custom marker to the map at given coordinates
+function addMarkerToMap(coords) {
+    L.marker(coords, { icon: myIcon }).addTo(map);
 }
 
 function resizeMap() {
@@ -30,20 +51,20 @@ function calculateBounds() {
 }
 
 let locations = [
-    { name: 'Clinic 1 (Orthopaedic)', coordinates: [-45.139036, -178.59375] },
+    { name: 'Clinic 1 (Orthopaedic)', coordinates: [-42.55308, -179.296875] },
     { name: 'Clinic 1a - lvl3 (Immunology)', coordinates: [33.137551, -120.234375] },
-    { name: 'Clinic 2 (Rheumatology/ECG)', coordinates: [21.893046, -180.703125] },
-    { name: 'Clinic 2a (Allergy/Lung)', coordinates: [22.543726, -151.171875] },
-    { name: 'Clinic 3 - lvl3 (Opthalmology/Eye)', coordinates: [-13.239945, -180] },
-    { name: 'Clinic 4 - lvl3 (Surgery/GI)', coordinates: [-13.239945, -180] },
-    { name: 'Clinic 4a - lvl3 (Ultrasound)', coordinates: [-13.239945, -180] },
+    { name: 'Clinic 2 (Rheumatology/ECG)', coordinates: [25.165173, -180.703125] },
+    { name: 'Clinic 2a (Allergy/Lung)', coordinates: [25.799891, -151.171875] },
+    { name: 'Clinic 3 - lvl3 (Opthalmology/Eye)', coordinates: [-10.239945, -180] },
+    { name: 'Clinic 4 - lvl3 (Surgery/GI)', coordinates: [-10.239945, -180] },
+    { name: 'Clinic 4a - lvl3 (Ultrasound)', coordinates: [-10.239945, -180] },
     { name: 'Clinic 5 (Ambulatory)', coordinates: [-37.210317, -266.484375] },
-    { name: 'Clinic 6 (Paediatric)', coordinates: [23.191346, -321.328125] },
-    { name: 'Clinic 7 - lvl 3 (Skin)', coordinates: [-23.885838, -284.765625] },
+    { name: 'Clinic 6 (Paediatric)', coordinates: [25.191346, -321.328125] },
+    { name: 'Clinic 7 - lvl 3 (Skin)', coordinates: [-19.311143, -289.6875] },
     { name: 'Clinic 8 - lvl 3 (Oral)', coordinates: [-12.554564, -334.6875] },
-    { name: 'Clinic 9 (UTC)', coordinates: [-56.609482, -266.484375] },
-    { name: 'Clinic 10 (ENT/Audiology)', coordinates: [18.646245, -206.015625] },
-    { name: 'Clinic 12 - lvl3 (GI/Renal)', coordinates: [-13.239945, -180] },
+    { name: 'Clinic 9 (UTC)', coordinates: [-52.609482, -268.484375] },
+    { name: 'Clinic 10 (ENT/Audiology)', coordinates: [25.799891, -209.53125] },
+    { name: 'Clinic 12 - lvl3 (GI/Renal)', coordinates: [-10.239945, -180] },
     { name: 'Clinic 14', coordinates: [33.087551, -382.5] },
     { name: 'Clinic 22', coordinates: [72.766074, 271.40625] },
     { name: 'Clinic 23 - lvl3', coordinates: [47.040182, 237.65625] },
@@ -55,30 +76,31 @@ let locations = [
     { name: 'Clinic 34', coordinates: [-23.935838, 381.09375] },
     { name: 'Clinic 42', coordinates: [-63.283627, 140.625] },
     { name: 'Clinic 43', coordinates: [-83.726943, 79.453125] },
+
     { name: 'ACCI', coordinates: [-54.162434, 243.984375] },
     { name: 'ATC X-Ray - lvl3', coordinates: [-49.837982, 366.328125] },
-    { name: 'Blood Tests', coordinates: [-42.082974, -309.375] },
+    { name: 'Blood Tests', coordinates: [-38.082974, -309.375] },
     { name: 'Breast Unit', coordinates: [-19.973349, 216.5625] },
     { name: 'Cardiology Diagnostic Tests', coordinates: [-42.60308, -136.40625] },
     { name: 'Careers', coordinates: [-58.077876, -19.6875] },
-    { name: 'Cleft Lip and Palatte - lvl3', coordinates: [-23.885838, -284.765625] },
+    { name: 'Cleft Lip and Palatte - lvl3', coordinates: [-18.646245, -291.09375] },
     { name: 'Clinical Neurophysiology - lvl3', coordinates: [-27.683528, -66.796875] },
     { name: 'Child Development Centre', coordinates: [-4.915833, 291.796875] },
     { name: 'CT and MRI imaging', coordinates: [11.817351, -123.046875] },
     { name: 'Day Surgery', coordinates: [-23.241346, 348.75] },
-    { name: 'DEXA - lvl3', coordinates: [-17.308688, -177.890625] },
+    { name: 'DEXA - lvl3', coordinates: [-9.102097, -181.40625] },
     { name: 'Endoscopy - lvl3', coordinates: [-49.837982, 366.328125] },
     { name: 'Eye Unit and Cataract', coordinates: [-50.289339, -388.125] },
     { name: 'Haematology', coordinates: [74.909392, -106.171875] },
-    { name: 'Haematology Day Unit', coordinates: [-83.829945, 111.09375] },
+    { name: 'Haematology Day Unit', coordinates: [-83.111071, 110.390625] },
     { name: 'Heart Clinic', coordinates: [-1.406109, 168.046875] },
     { name: 'Lewin Unit', coordinates: [70.377854, 16.875] },
     { name: 'Lysosomal Unit', coordinates: [78.490552, 8.4375] },
     { name: 'Medical Photography - lvl3', coordinates: [-17.308688, -336.09375] },
     { name: 'Medical Records - lvl3', coordinates: [-20.632784, -284.765625] },
-    { name: 'MRIs', coordinates: [80.297927, 51.328125] },
+    { name: 'MRIs', coordinates: [80.983688, 49.921875] },
     { name: 'NCCU', coordinates: [-10.487812, -63.984375] },
-    { name: 'Neonatal Unit - lvl3', coordinates: [60.239811, 243.984375] },
+    { name: 'Neonatal Unit - lvl3', coordinates: [64.774125, 242.578125] },
     { name: 'Nuclear Medicine', coordinates: [-59.225928, -158.203125] },
     { name: 'Oncology', coordinates: [63.810036, -104.765625] },
     { name: 'Pain Clinic - lvl3', coordinates: [-17.308688, -336.09375] },
@@ -131,7 +153,7 @@ function addLocations() {
 
 function createMarkers() {
     locations.forEach(location => {
-        let marker = L.marker(location.coordinates);
+        let marker = L.marker(location.coordinates, { icon: myIcon });
         markers.push(marker);
     });
 }
